b6bd2ae3632f0c84548eb538a2cf4f6cf59b106a
nattob <132520498+nattob@users.noreply.github.com>
2024-08-20 20:41:31 -0700

    add vs sln

---
+++ code/game/game.cpp
@@ -0,0 +1,10 @@
+// game.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <windows.h>
+
+int WINAPI WinMain( _In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd )
+{
+    OutputDebugStringA( "Wow! Print! \n" );
+    return 0;
+}
